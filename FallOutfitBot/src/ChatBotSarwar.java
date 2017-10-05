847168f7980fe612570dc3b1ac6c52b0a2224676
BT_1N3_03 <BT_1N3_03@BT_1N3_03.bktech.local>
2017-10-05 13:16:23 -0400

    changes to chatbotsarwar

--- FallOutfitBot/src/ChatBotSarwar.java
+++ FallOutfitBot/src/ChatBotSarwar.java
@@ -1,4 +1,47 @@
 
 public class ChatBotSarwar {
+	public String Start()
+	{
+		return "Let's start with a top.";
+	}
+	private int findKeyword(String statement, String goal,int startPos)
+	{
+		String phrase = statement.trim().toLowerCase();
+		goal = goal.toLowerCase();
 
+		int psn = phrase.indexOf(goal, startPos);
+		
+		while (psn >= 0)
+		{
+			
+			String before = " ", after = " ";
+			if (psn > 0)
+			{
+				before = phrase.substring(psn - 1, psn);
+			}
+			if (psn + goal.length() < phrase.length())
+			{
+				after = phrase.substring(
+						psn + goal.length(),
+						psn + goal.length() + 1);
+			}
+
+			if (((before.compareTo("a") < 0) || (before
+					.compareTo("z") > 0)) 
+					&& ((after.compareTo("a") < 0) || (after
+							.compareTo("z") > 0)))
+			{
+				return psn;
+			}
+
+			psn = phrase.indexOf(goal, psn + 1);
+
+		}
+
+		return -1;
+	}
+	private int findKeyword(String statement, String goal)
+	{
+		return findKeyword (statement, goal, 0);
+	}
 }

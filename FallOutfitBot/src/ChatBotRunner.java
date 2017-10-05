43e4555019fd28f8488a905e674f53c6cded63b5
BT_1N3_02 <BT_1N3_02@BT_1N3_02.bktech.local>
2017-10-05 13:13:50 -0400

    Runner updated, changes made to ChatBotMonroe

--- FallOutfitBot/src/ChatBotRunner.java
+++ FallOutfitBot/src/ChatBotRunner.java
@@ -2,8 +2,8 @@ import java.util.Scanner;
 
 /**
  * A simple class to run our chatbot teams.
- * @author Mr. Levin
- * @version September 2017
+ * @author 
+ * @version October 2017
  */
 public class ChatBotRunner
 {
@@ -13,19 +13,61 @@ public class ChatBotRunner
 	 */
 	public static void main(String[] args)
 	{
-		ChatBotLevin chatbot1 = new ChatBotLevin();
-		
-		System.out.println (chatbot1.getGreeting());
+
+		System.out.println ("Welcome to Fall Outfit Bot, a bot that recommends you fall themed outfits based on your preferences. Are you looking for male, female, or unisex clothes?");
 		Scanner in = new Scanner (System.in);
-		String statement = in.nextLine();
 		
-
-
-		while (!statement.equals("Bye"))
+		String statement = in.nextLine();
+		String gender = "";
+		
+		if(statement.toLowerCase() == "male")
 		{
-			System.out.println (chatbot1.getResponse(statement));
+			gender = "male";
+		}
+		else if(statement.toLowerCase() == "female")
+		{
+			gender = "female";
+		}
+		else
+		{
+			gender = "unisex";
+		}
+
+		
+		System.out.println("Thank you. Would you like to start with tops, bottoms, shoes, or accessories?");
+		statement = in.nextLine();
+		
+		if(statement.toLowerCase() == "tops")
+		{
+			ChatBotSarwar chatbot1 = new ChatBotSarwar();
 			statement = in.nextLine();
 		}
+		/*
+		if(statement.toLowerCase() == "bottoms")
+		{
+			ChatBotLamia chatbot2 =  new ChatBotLamia();
+			statement = in.nextLine();
+		}
+		*/
+		if(statement.toLowerCase() == "shoes")
+		{
+			ChatBotKaitlyn chatbot3 = new ChatBotKaitlyn();
+			statement = in.nextLine();
+		}
+		if(statement.toLowerCase() == "accessories")
+		{
+			System.out.println("HECK");
+			ChatBotMonroe chatbot4 = new ChatBotMonroe();
+			while(!statement.equals("Bye"))
+			{
+				System.out.println(chatbot4.getResponse(statement));
+				statement = in.nextLine();
+				
+			}
+			
+			
+		}
+
 	}
 
 }
